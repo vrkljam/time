791794c0a95804fe1cd1653dcf6d6aa74465ff58
vrkljam <vrkljam@earthlink.net>
2023-12-08 13:59:39 -0700

    first version of time clock with international times

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-import moment from "moment";
+const allzone = document.getElementById("allzone");
+const localDate = document.getElementById("local-date");
+const intTime = document.getElementById("int-time");
+const intTime2 = document.getElementById("int-time2");
+const boulder = document.getElementById("boulder");
+const aussie = document.getElementById("aussie");
 
-moment().format();
+function interTime() {
+  let x = new Date();
+  let boulderTime = new Intl.DateTimeFormat("en-US", {
+    timeZone: "America/Denver",
+    dateStyle: "full",
+    timeStyle: "medium",
+  });
+
+  let aussieTime = new Intl.DateTimeFormat("en-US", {
+    timeZone: "Australia/Sydney",
+    dateStyle: "full",
+    timeStyle: "medium",
+  });
+  boulder.innerText = boulderTime.format(x);
+  aussie.innerText = aussieTime.format(x);
+}
+
+setInterval(() => {
+  interTime(), 1000;
+});
